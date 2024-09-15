4163e9647c254c91e5e0f960171574ea0eb03a8a
rm93586 <barrence.santos@gmail.com>
2024-09-15 02:00:06 -0300

    Fix Settings Port Number Event Save Handling Bug
    * Fixed the issue where an event was being triggered after every key press in the settings form. This bug prevented users from changing the value of the Port Number field. With this fix, users can now update the settings as expected.

--- src/settings.tsx
+++ src/settings.tsx
@@ -10,39 +10,73 @@ import { AppContext } from "./utils/app-context";
 const Settings = () => {
   // @ts-ignore
   const { fileBrowserManager } = useContext(AppContext);
-  const [port, setPort] = useState(fileBrowserManager.getPort());
+  const [port, setPort] = useState( null );
   const [isSaving, setIsSaving] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [invalidPortError, setInvalidPortError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleSave = useCallback(async () => {
+    await fileBrowserManager.fileBrowserSendLogInfo("Attempting to save port value: " + port);
+    await fileBrowserManager.fileBrowserSendLogInfo("Has errors: " + invalidPortError);
     setIsSaving(true);
-    await fileBrowserManager.setPort(port);
+    if(!invalidPortError) {
+      await fileBrowserManager.setPort(port);
+      showSuccess("Port Number saved successfully.");
+    }
+    
     setIsSaving(false);
-  }, [port]);
+  }, [port, invalidPortError]);
 
   const handlePortChange = (e) => {
+
+    showError("");
+    setInvalidPortError(true);
+
+    const isNumeric = !isNaN(+e.target.value)
+    if (!isNumeric) {
+      return;
+    }
     const portNumber = +e.target.value;
+
     // Decky uses port 1337
-    // To use a port equal or lower than 1024 on Linux, you need root access
-    if (portNumber > 1024 && portNumber !== 1337) {
-      setInvalidPortError(false);
-      setPort(e.target.value);
+    if (portNumber == 1337) {
+      showError("The port number 1337 cannot be used.")
       return;
     }
 
-    setInvalidPortError(true);
+    // To use a port equal or lower than 1024 on Linux, you need root access
+    if((portNumber < 1024)  || (portNumber > 65535)) {
+      return;
+    }
+
+    setPort(portNumber);
+    setInvalidPortError(false);
+  };
+
+  const showError = (message: string) => {
+    setSuccessMessage("");
+    setErrorMessage(message);
+  };
+
+  const showSuccess = (message: string) => {
+    setErrorMessage("");
+    setSuccessMessage(message);
+    setTimeout(() => {
+      setSuccessMessage("");
+    }, 3000);
   };
 
   useEffect(() => {
     const loadDefaults = async () => {
+      setIsLoading(true);
       const _port = await fileBrowserManager.getPortFromSettings();
       setPort(_port);
+      setIsLoading(false);
     };
 
-    setIsLoading(true);
-    loadDefaults();
-    setIsLoading(false);
+    loadDefaults();  
   }, []);
 
   return (
@@ -56,16 +90,29 @@ const Settings = () => {
           <PanelSectionRow>
             <TextField
               label="Port"
-              description="TCP port used for connection"
+              description="TCP port used for connection. The port number 1337 cannot be used because of decky."
               mustBeNumeric
               rangeMin={1025}
               rangeMax={65535}
               onChange={handlePortChange}
-              value={port}
               style={{
                 border: invalidPortError ? "1px red solid" : undefined,
               }}
             />
+            {errorMessage && (
+            <PanelSectionRow>
+              <div style={{ color: 'red', fontWeight: '300' }}>
+                {errorMessage}
+              </div>
+            </PanelSectionRow>
+            )}
+            {successMessage && (
+            <PanelSectionRow>
+              <div style={{ color: 'green', fontWeight: 'bold' }}>
+                {successMessage}
+              </div>
+            </PanelSectionRow>
+            )}
           </PanelSectionRow>
           <PanelSectionRow>
             <ButtonItem onClick={handleSave} disabled={isSaving}>
