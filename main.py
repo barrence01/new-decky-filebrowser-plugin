060fcafa964183ee7d7f393f1c856e5eee588594
William Barrence <barrence.santos@gmail.com>
2025-07-19 08:07:59 -0300

    Added self-repair in case of failure. The plugin will try to reset the settings and recreate the database.

--- main.py
+++ main.py
@@ -91,6 +91,8 @@ class Plugin:
                 decky_plugin.logger.error("The server could not be started.")
                 stdout = await process.stdout.read(300)
                 decky_plugin.logger.error(stdout.decode("utf-8"))
+                decky_plugin.logger.error("Maybe there's something wrong with the DB? The DeckyFileBrowser will try to self repair.")
+                self.check_settings(self)
                 return {
                     "status": "offline",
                     "output": "none"
